7cebe24e5c1b66ebf44237b5f44952246ebe06fd
Prakash meena <meenaprak12@gmail.com>
2025-04-05 15:06:40 +0530

    update

--- src/app/[pokemon]/page.tsx
+++ src/app/[pokemon]/page.tsx
@@ -13,8 +13,9 @@ async function getPokemonData(pokemon: string): Promise<any> {
   return res.json();
 }
 
-export default async function PropertyDetailPage({ params: { pokemon } }: PageProps) {
-  const data = await getPokemonData(pokemon);
+export default async function PropertyDetailPage({ params }: PageProps) {
+    const pokemon = params.pokemon;
+    const data = await getPokemonData(pokemon);
 
   return (
     <div className="max-w-3xl mx-auto p-4">
